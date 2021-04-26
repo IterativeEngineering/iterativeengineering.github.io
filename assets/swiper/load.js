1d1b942fd4ad0196a526943981287eef524ce2e0
Adam Drozdz <adam.drozdz@iterative.pl>
2021-04-26 14:17:12 +0200

    v1.0.19

--- assets/swiper/load.js
+++ assets/swiper/load.js
@@ -1,25 +1,65 @@
-var testimonialSwiper = new Swiper('#testimonial-swiper', {
-  direction: 'horizontal',
-  loop: true,
-  speed: 600,
-  autoplay: {
-    delay: 6500,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+window.addEventListener('DOMContentLoaded', () => {
+    initSwiper();
 });
 
-var projectsSwiper = new Swiper('#projects-swiper', {
-  direction: 'horizontal',
-  loop: true,
-  speed: 600,
-  autoplay: {
-    delay: 30000,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-});
+function initSwiper() {
+  const slideDesc = document.getElementsByClassName('slide-desc');
+  let initSlide = 0;
+  let prevIndex = 0;
+  
+  const testimonialSwiper = new Swiper('#testimonial-swiper', {
+    direction: 'horizontal',
+    loop: true,
+    speed: 600,
+    autoplay: {
+      delay: 6500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+  
+  const projectsSwiper = new Swiper('#projects-swiper', {
+    init: false,
+    direction: 'horizontal',
+    loop: true,
+    speed: 600,
+    autoplay: {
+      delay: 30000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  
+  
+  projectsSwiper.on('init', () => {
+    slideDesc[initSlide].className += ' active';
+    slideDesc[initSlide].removeAttribute('hidden');
+  }) 
+  
+  projectsSwiper.init();
+  
+  projectsSwiper.on('realIndexChange', () => {
+    slideDesc[projectsSwiper.realIndex].removeAttribute('hidden');
+
+    // Force DOM rerender to allow animations to work on rerendered elements
+    slideDesc[projectsSwiper.realIndex].clientWidth;
+
+    slideDesc[projectsSwiper.realIndex].classList.add('active');
+    
+    slideDesc[prevIndex].classList.remove('active');
+    slideDesc[prevIndex].setAttribute('hidden', true);
+
+    prevIndex = projectsSwiper.realIndex;
+  });
+}
+
